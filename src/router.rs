609a9a333088db58d278d28fedcef16bffe2771a
Gints Polis <polis.gints@gmail.com>
2024-06-02 09:04:56 +0300

    frontend

--- src/router.rs
+++ src/router.rs
@@ -7,5 +7,5 @@ pub async fn build_routes() -> Router {
     Router::new()
         .route("/api/login", post(login_handler))
         .route("/api/info", get(get_info_handler))
-        .route_service("/", get_service(ServeDir::new("./web")))
+        .fallback_service(get_service(ServeDir::new("./web/dist")))
 }
