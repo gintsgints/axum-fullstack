3d8e1dab493a56570add0643dcf8c86295bc5e39
Gints Polis <polis.gints@gmail.com>
2024-06-10 02:52:57 +0300

    Add vuetify npm

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,24 @@
 <script setup lang="ts">
+import { ref } from "vue"
 import HelloWorld from './components/HelloWorld.vue'
+
+const drawer = ref(false);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld msg="Vite + Vue" />
+    </v-main>
+  </v-app>
+</template>
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
-import './style.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+})
+
+createApp(App).use(vuetify).mount('#app')
